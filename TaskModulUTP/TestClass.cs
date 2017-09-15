33a72784fe723d165426a519e76a67bffab3de8d
Martynov Yaroslav <martynovyaroslav88@gmail.com>
2017-09-15 08:00:48 +0300

    work_repeat

--- TaskModulUTP/TestClass.cs
+++ TaskModulUTP/TestClass.cs
@@ -17,36 +17,35 @@ namespace GuiTest
     public class UnitTest1 
     {
 
-
-        IWebDriver driver = new ChromeDriver();
-
-        string url = "https://www.google.com/intl/ru/gmail/about/";
+        private IWebDriver _driver;
 
 
         [SetUp]
         public void BeaforTest()
         {
 
+            _driver = new ChromeDriver();
+            string url = "https://www.google.com/intl/ru/gmail/about/";
+            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMilliseconds(5000));
+            _driver.Navigate().GoToUrl(url);
 
-            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMilliseconds(5000));
-            driver.Navigate().GoToUrl(url);
+            string path = Directory.GetCurrentDirectory();
 
-            string fileName = Path.Combine("D:\\1\\ModulAutomation\\TaskModulUTP\\Data", "Login.xlsx");
+            string fileName = Path.Combine("d:\\automation\\epam project\\Stability\\Data\\", "Login.xlsx");
             ExcelHelpers.PopulateInCollection(fileName);
         }
 
 
 
-        [TestCase(1)]
-        [Retry(3)]
+        [TestCase(1), Repeat(2)]
         //[Retry(Times = 3, RequiredPassCount = 2)]
         public void AuthorisationByUser(int numm)
         {
 
 
-            LandingPage landingPage = new LandingPage(driver);
-            SignInGmail signInGmail = new SignInGmail(driver);
-            SignInPassword signInPassword = new SignInPassword(driver);
+            LandingPage landingPage = new LandingPage(_driver);
+            SignInGmail signInGmail = new SignInGmail(_driver);
+            SignInPassword signInPassword = new SignInPassword(_driver);
 
 
             landingPage.PressEnterLandingPage();
@@ -60,7 +59,7 @@ namespace GuiTest
         [TearDown]
         public void AfterTest()
         {
-            driver.Quit();
+            _driver.Quit();
         }
     }
 }
